e5ddc1159c7f78d8db89507fc69614bb8de09478
jshoe54 <60626993+jshoe54@users.noreply.github.com>
2020-03-02 14:47:24 -0700

    Add files via upload

---
+++ SortOfSort.java
@@ -0,0 +1,14 @@
+
+public class SortOfSort {
+
+	
+	public int sortOfSort(int array []) {
+		for(int i=0;i<array.length;i++) {
+			int max = array[i];
+			if(max<array[i]) {
+				max = array[i];
+			}
+		}
+		
+	}
+}
